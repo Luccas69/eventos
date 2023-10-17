2d019ffb70ae2e3d0ea211d6a4be223b1cb85793
Luccas69 <luccas.rossarolla@gmail.com>
2023-10-16 20:10:40 -0400

    criação do cabeçalho

---
+++ src/components/Cabecalho/index.jsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.css';
+
+export default function Cabecalho() {
+
+    return (
+        <header className={styles.header}>
+            <h1>Meu site</h1>
+        </header>
+    )
+};
