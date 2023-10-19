3be7b8973f494e4127a0c575bc42905df1b07cf2
Luccas69 <luccas.rossarolla@gmail.com>
2023-10-18 22:06:16 -0400

    alterando eventos

--- src/components/Card /index.jsx
+++ src/components/Card /index.jsx
@@ -1,16 +1,16 @@
 import Link from "next/link"
 import styles from './styles.module.css'
 
-export default function Card ({id, titulo, dataInicio, dataFim, local, imagem}) {
+export default function Card({ id, titulo, dataInicio, dataFim, local, imagem }) {
 
     return (
         <div className={styles.Card}>
             <Link href={`/eventos/${id}`}>
+                <img src={imagem} alt="uma img qualquer" />
                 <h2 className={styles.cardTitulo}>{titulo}</h2>
                 <div>Data Inicio:{dataInicio}</div>
                 <div>Data Fim: {dataFim}</div>
                 <div>Local: {local}</div>
-                <img src={imagem} alt="uma img qualquer" />
             </Link>
         </div>
     )
